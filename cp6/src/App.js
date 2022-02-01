ae120b7bd10f177b68dabde60a74e517fa6e49f8
richardmarrugo17 <richard17_m@hotmail.com>
2022-02-01 13:55:22 -0500

    commit final

--- cp6/src/App.js
+++ cp6/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
+import ProyectList from './components/ProyectList';
 
 function App() {
+  const Lista_datos = [
+    {id:"1", nombre:"Sistema de Control de Reservas", fecha: "11/12/2021", descripcion: "Sistema para controlar las reservas y facturación a huespedes de un hotel.", director: "MisionTic 2022"},
+    {id:"2", nombre:"Sistema de Control de Inventarios", fecha: "11/12/2021", descripcion: "Sistema para controlar inventarios de bodegas con multiples ubicaciones.", director: "MisionTic 2022"},
+    {id:"3", nombre:"Sistema de Reconocimiento Facial de Mascarillas", fecha: "11/12/2021", descripcion: "Sistema para reconocer si las personas traen o no puesta la mascarilla.", director: "MisionTic 2022"},
+    {id:"4", nombre:"Sistema de Control de Riego de Cultivos", fecha: "11/12/2021", descripcion: "Servicio Restful para dispositivos IoT que activan o desactivan los sistemas de riego de cultivos.", director: "MisionTic 2022"},
+    {id:"5", nombre:"Sistema de Control de Temperatura", fecha: "11/12/2021", descripcion: "Servicio RESTful para dispositivos IoT que emite alertas cuando la temperatura de los contenedores sale de los umbrales permitidos", director: "MisionTic 2022"},
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBar/>
+      <ProyectList proyectos={ Lista_datos }/>
+      <footer className="bg-dark text-white mt-5 mt-lg-3">
+          <div className="container-fluid">
+              <div className="card bg-dark">
+                  <div className="card-body">
+                      <h2>Republica de Colombia</h2>
+                      <h4>Ministerio de las Tic</h4>
+                      <p>Dirección: AV. 000 No. 00 - 00</p>
+                      <p>Telefono: 000 0000</p>
+                      <p>Bogota - Colombia</p>
+                  </div>
+              </div>
+          </div>
+      </footer>
     </div>
-  );
+    );
 }
 
 export default App;
---
+++ cp6/src/components/ProyectList.js
@@ -0,0 +1,34 @@
+import ProyectListItem from "./ProyectListItem";
+import Paginator from "./Paginator";
+function ProyectList(props){
+  const proyectos = props.proyectos;
+  const ListaProyectos = proyectos.map(p =>
+      <ProyectListItem
+        id={p.id}
+        nombre={p.nombre}
+        descripcion={p.descripcion}
+        fecha={p.fecha}
+        director={p.director}
+      />
+  );
+  return (
+    <div className="container-fluid">
+      <div className="card mt-5">
+        <div className="card-body">
+          <button id="btnRegistrarNov" className="btn btn-lg btn-success float-end">Registrar Novedades</button>
+          <h3>Dashboard</h3>
+          <h5 className="text-muted">Bienvenido al portal de MINTIC de Proyectos y Noticias</h5>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto asperiores sunt 
+            expedita numquam, consequatur aut ab, corrupti aliquid eius blanditiis omnis veritatis 
+            dignissimos quo atque veniam doloremque esse assumenda magni!</p>
+        </div>
+      </div>
+      <div className="list-group m-5">
+        {ListaProyectos}
+      </div>
+      <Paginator/>
+    </div>
+  );   
+}
+
+export default ProyectList;
