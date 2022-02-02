4272a7ac5530ae627c6a82a5011c45f5ae7d96a0
richardmarrugo17 <richard17_m@hotmail.com>
2022-02-02 16:58:38 -0500

    commit final

--- cp6_b/src/App.js
+++ cp6_b/src/App.js
@@ -1,4 +1,4 @@
-import React, {Fragment, useState, useRef} from "react";
+import React, {Fragment, useState, useRef, useEffect} from "react";
 import { v4 as uuidv4} from "uuid";
 import { TodoList } from "./components/TodoList";
 
@@ -10,6 +10,16 @@ export function App(){
     ]);
     // Referencia para obtener a la data ingresada y usarla en el handle
     const taskRef = useRef();
+    // esta funcion es para manejar el Checkbox
+    const toggleTask = (id) => {
+        // copia de las tareas
+        const newTasks = [...listas];
+        //encontrar la tarea seleccionada segun su id
+        const task = newTasks.find((task) => task.id === id);
+        // si la tarea esta completa lo convierte en no completa y si No esta completa la convierte en completa
+        task.completed = !task.completed;
+        setListas(newTasks); // Actualizamos el listado de las tareas
+    };
     //Metodo para añadir tareas
     const handleTaskAdd = () =>{
         //crear la referencia de la tarea
@@ -23,13 +33,36 @@ export function App(){
         });
         taskRef.current.value = null; // limpiar el input cuando se añade
     };
+    //Manejador de eliminar tareas
+    const handleClearAll = () =>{
+        // se hace una copia de las tareas creadas y
+        //se filtra por aquellas que han sido seleccionadas
+        const newTasks = listas.filter((task) => !task.completed);
+        //setListas setea los elementos
+        setListas(newTasks);
+    }
+    // Pare escuchar y guardar las nuevas tareas
+    useEffect (() => {
+        localStorage.setItem("listApp.lists", JSON.stringify(listas));
+
+    }, [listas]);
+    //Para visualizar aquellas que ya se encuentren creadas
+    useEffect(() => {
+        const storedTasks = JSON.parse(localStorage.getItem("listApp.lists"));
+        if (storedTasks){
+            setListas(storedTasks);
+        }
+    },[]);
     return (
         //Fragment se utiliza para agrupar varios elementos
         <Fragment>
-            <TodoList listas={listas}/>
+            <TodoList listas={listas} toggleTask={toggleTask}/>
             <input ref={taskRef} type="text" placeholder="Nueva Tarea"/>
             <button onClick={handleTaskAdd}>Agregar</button>
-            <button>Eliminar</button>
+            <button onClick={handleClearAll}>Eliminar</button>
+            <div>
+                <p>Te quedan {listas.filter((task) => !task.completed).length} tareas por terminar.</p>
+            </div>
         </Fragment>
     );
 }
--- cp6_b/src/components/TodoItem.js
+++ cp6_b/src/components/TodoItem.js
@@ -1,6 +1,16 @@
 import React from "react";
 
-export function TodoItem({lista}) {
-    const {id, task, complete} =lista;
-    return <li>{task}</li>;
+export function TodoItem({ lista, toggleTask }) {
+    const {id, task, completed} =lista;
+    //Manejador del click de las tareas para
+    //para llamar la funcion toggleTask de App.js
+    const handleTaskClick = () =>{
+        toggleTask(id);
+    };
+
+    return (
+    <li>
+        <input type={"checkbox"} checked={completed} onChange={handleTaskClick}/>{task}
+    </li>
+    );
 }
